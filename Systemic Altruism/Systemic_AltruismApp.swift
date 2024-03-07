f8bbe28e076a2482be4eff32ec3a140862c9ec54
Nishant <nishantminerva@gmail.com>
2024-03-07 21:33:58 +0530

    Initial Commit

---
+++ Systemic Altruism/Systemic_AltruismApp.swift
@@ -0,0 +1,17 @@
+//
+//  Systemic_AltruismApp.swift
+//  Systemic Altruism
+//
+//  Created by Nishant Minerva on 07/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Systemic_AltruismApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
